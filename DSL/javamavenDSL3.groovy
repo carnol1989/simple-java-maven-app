640a977ac0ac06f7693ea76b6e3ad9264cc6f602
Carlos Nole <carnole1989@outlook.com>
2023-05-30 22:27:04 -0500

    chore: update info repo

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL 3') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/carnol1989/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('carnol1989')
+            node / gitConfigEmail('carnole1989@outlook.com')
         }
     }
     triggers {
@@ -11,11 +11,11 @@ job('Java Maven App DSL 3') {
     }    
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('udemy-jenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('udemy-jenkins')
           goals('test')
         }
         shell('''
@@ -49,9 +49,9 @@ job('Java Maven App DSL 3') {
 job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	scm {
-		git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-		    node / gitConfigName('macloujulian')
-		    node / gitConfigEmail('macloujulian@gmail.com')
+		git('https://github.com/carnol1989/simple-java-maven-app.git', 'master') { node ->
+		    node / gitConfigName('carnole1989')
+		    node / gitConfigEmail('carnole1989@outlook.com')
 		}
 	}
 	triggers {
